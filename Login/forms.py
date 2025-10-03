946ae12567b7931a1f9df4919b98aeeeee259a65
Isabelle-Matos <isabelle.mattos25@aluno.ufsj.edu.br>
2025-10-03 10:13:42 -0300

    login-models

---
+++ Login/forms.py
@@ -0,0 +1,40 @@
+# home/forms.py
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'w-full p-2 border rounded',
+            'placeholder': 'Username'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-full p-2 border rounded',
+            'placeholder': 'Password'
+        })
+    )
+
+class RegisterForm(UserCreationForm):
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class': 'w-full p-2 border rounded',
+        'placeholder': 'Email'
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'w-full p-2 border rounded',
+        'placeholder': 'Username'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full p-2 border rounded',
+        'placeholder': 'Password'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full p-2 border rounded',
+        'placeholder': 'Confirm Password'
+    }))
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
