f2fdd6e8c3fc9b33917835d2f3b5fe083ff83444
Isabelle-Matos <isabelle.mattos25@aluno.ufsj.edu.br>
2025-10-23 13:51:45 -0300

    fixed transactions/models

--- Transactions/models.py
+++ Transactions/models.py
@@ -1,47 +1,63 @@
 from django.db import transaction
 from Persistence.services.client_service import ClientService
 from Persistence.services.transaction_service import TransactionService
+from Persistence.services.category_service import CategoryService
 
 class FinancialTransactionProcessor:
-   
     def __init__(self):
         self.client_service = ClientService()
         self.transaction_service = TransactionService()
+        self.category_service = CategoryService()
 
-    @transaction.atomic
-    def process_transaction(self, user, income=0, expense=0,
-                            income_category=None, expense_category=None,
-                            action="register"):
+    def register_income(self, user, value, category, description=None, frequency=None):
+        """Register a new income transaction"""
+        client = self.client_service.get_client(client=user)
+        
+        income = self.transaction_service.register_income(
+            client=user,
+            value=value,
+            category=category,
+            description=description,
+            frequency=frequency
+        )
+        
+        new_balance = client.current_balance + value
+        self.client_service.update_balance(client, new_balance)
+        return income
 
+    def register_outgoing(self, user, value, category, description=None):
+        """Register a new outgoing transaction"""
 
-        try:
-            client = self.client_service.get_client(client=user)
-        except:
-            client = self.client_service.create_client(user)
+        client = self.client_service.get_client(client=user)
+        outgoing = self.transaction_service.register_outgoing(
+            client=user,
+            value=value,
+            category=category,
+            description=description
+        )
+        
+        new_balance = client.current_balance - value
+        self.client_service.update_balance(client, new_balance)
+        return outgoing
 
-        result = {}
+    def get_transactions(self, user):
+        """Get all transactions for a user"""
+        income_transactions = self.transaction_service.get_incomes_from_user(user)
+        outgoing_transactions = self.transaction_service.get_outgoings_from_user(user)
+        return income_transactions, outgoing_transactions
 
-        if action == "register":
-            if expense > income:
-                expense = income
+    def get_categories_by_type(self, type_name):
+        """Get categories by type"""
+        return self.transaction_service.get_category_by_type(type_name)
 
-            if income > 0 and income_category:
-                self.transaction_service.register_income(user, income, income_category)
-
-            if expense > 0 and expense_category:
-                self.transaction_service.register_outgoing(user, expense, expense_category)
-
-            new_balance = client.current_balance + income - expense
-            self.client_service.update_balance(client, new_balance)
-
-            result["status"] = "registered"
-            result["current_balance"] = new_balance
-
-        elif action == "simulate":
-            result["balance"] = income - expense
-            result["description"] = "Simulation"
-
+    def update_transaction_category(self, transaction_type, transaction_id, category):
+        """Update transaction category"""
+        if transaction_type == 'income':
+            transaction = self.transaction_service.get_income_by_id(transaction_id)
+            if transaction:
+                self.transaction_service.update_income(transaction, **{'category': category})
         else:
-            raise ValueError("Invalid action")
-
-        return result
+            transaction = self.transaction_service.get_outgoing_by_id(transaction_id)
+            if transaction:
+                self.transaction_service.update_outgoing(transaction, **{'category': category})
+        return transaction
