3f9ad226a7158b27042c5bff709f887689324750
Isabelle-Matos <isabelle.mattos25@aluno.ufsj.edu.br>
2025-10-30 21:01:52 -0300

    setup model-view integration with persistence

--- Transactions/views.py
+++ Transactions/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from Persistence.services.category_service import CategoryService
 import logging
+from django.contrib import messages
+
 
 def add_income(request):
     if not request.user.is_authenticated:
@@ -17,8 +19,8 @@ def add_income(request):
         if form.is_valid():
             # income = form.save(commit=False)
             income = form.cleaned_data
-            tmp = TransactionService()
-            tmp.register_income(client=request.user, value=income['value'], category=income['category'], description=income['description'], frequency=income['frequency'])
+            tmp = FinancialTransactionProcessor()
+            tmp.register_income(user=request.user, value=income['value'], category=income['category'], description=income['description'], frequency=income['frequency'])
             return redirect('home:dashboard')
         else:
             return HttpResponse(form.errors)
@@ -31,16 +33,16 @@ def add_outgoing(request):
     if not request.user.is_authenticated:
         return redirect('login')
 
-    categories = TransactionService().get_category_by_type('outgoing')
-
     if request.method == 'POST':
         form = OutgoingTransactionForm(request.POST)
         if form.is_valid():
             # outgoing = form.save(commit=False)
             outgoing = form.cleaned_data
-            tmp = TransactionService()
-            tmp.register_outgoing(client=request.user, value=outgoing['value'], category=outgoing['category'], description=outgoing['description'], frequency=outgoing['frequency'])
+            tmp = FinancialTransactionProcessor()
+            tmp.register_outgoing(user=request.user, value=outgoing['value'], category=outgoing['category'], description=outgoing['description'])
             return redirect('home:dashboard')
+        else:
+            return HttpResponse(form.errors)
     else:
         form = OutgoingTransactionForm()
 
@@ -51,41 +53,32 @@ def view_all(request):
         return redirect('login')
     
     user = request.user
-    service = TransactionService()
+    service = FinancialTransactionProcessor()
 
     if request.method == 'POST':
         action = request.POST.get('action')
-        category_service = CategoryService()
+        category_service = FinancialTransactionProcessor()
         transaction_type = request.POST.get('transaction_type')
         transaction_id = request.POST.get('transaction_id')
 
-        if transaction_type == 'income':
-            transaction = service.get_income_by_id(transaction_id)
-        else:
-            transaction = service.get_outgoing_by_id(transaction_id)
-
         if action == 'delete':
-            if transaction_type == 'income':
-                service.delete_income(transaction)
-            else:
-                service.delete_outgoing(transaction)
-    
-            return redirect('view_all')
+            service.delete_transaction(request.user, transaction_type, transaction_id)
+            messages.success(request, f'Transação deletada com sucesso!')
+            
 
         elif action == 'update':
             new_category_id = request.POST.get('category')
-            category = category_service.get_category_by_id(new_category_id)
-            if transaction and category:
-                transaction.category = category
-                if transaction_type == 'income':
-                    service.update_income(transaction, **{'category': category})
-                else:
-                    service.update_outgoing(transaction, **{'category': category})
+            category = category_service.get_categories_by_id(new_category_id)
+            if category:
+                try:
+                    service.update_transaction_category(transaction_type, transaction_id, category)
+                    messages.success(request, f'Transação atualizada com sucesso!')
+                except category.DoesNotExist:
+                    messages.error(request, "Não foi possível atualizar a Transação")
 
-            return redirect('view_all')
+        return redirect('view_all')
 
-    income_transactions = service.get_incomes_from_user(user)
-    outgoing_transactions = service.get_outgoings_from_user(user)
+    income_transactions, outgoing_transactions = service.get_transactions(user)
     
 
     grouped = {}
